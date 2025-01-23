6a40fb3ed4623e334b9de0d8609d072f9c544957
Lufine <lfneuwirth@gmail.com>
2025-01-23 09:16:20 -0300

    gerenciador de times

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function navigateTo(page) {
-    if (page === 'index') {
+    if (page === 'TeamBalancer') {
         window.location.href = './html/TeamBalancer.html';
     } else if (page === 'game') {
         window.location.href = './html/game.html';
